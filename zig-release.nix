7c4ff091f1793d35ca1eefff651d4ca1e5a6ff5d
Erik Arvstedt <erik.arvstedt@gmail.com>
2025-10-31 14:24:44 +0100

    update to Zig `0.16.0-dev.932+6568f0f75` [auto]

--- zig-release.nix
+++ zig-release.nix
@@ -1,11 +1,11 @@
 {
-  version = "0.16.0-dev.923+06d9e3bc0";
+  version = "0.16.0-dev.932+6568f0f75";
   src = {
-    rev = "06d9e3bc065be1614ff8e01e394a5160d7283dd7";
-    hash = "sha256-jyEtKlFivplKVMFW3+WovIASzSxVW0RodhwdJmhmSgk=";
+    rev = "6568f0f75b900d2ee27363b2ba81bedf206a13e7";
+    hash = "sha256-2fnHV+clib9aqcgppDaV4u/jK2XlG8Vy50xQimDk83o=";
   };
   binaries = {
-    x86_64-linux.sha256 = "07b0cb070d91f3a0e03cd55505be8d8b21f1e09e64414d53f0089ab002658ea6";
-    aarch64-linux.sha256 = "5b67a980fc69f93cfdc122eed7f403449aa1007c7e8e14b2604276d532a45bbd";
+    x86_64-linux.sha256 = "c32d3715ac54d3a68700d08fc55699292c56e1061f5bf3f51fd71049b6357708";
+    aarch64-linux.sha256 = "e401d4560b1b3db1c8d1f3de053a0dd1c7cc693f319274bf1623d74dfe306cd5";
   };
 }
